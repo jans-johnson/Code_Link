49838e8bbc819eaf18b96cbd1c455d60500daac8
Jans Johnson <jaansjohnson@gmail.com>
2022-06-21 15:26:58 +0530

    Bottom Navigation Bar

--- app/src/main/java/com/jns/codelink/activities/MainActivity.kt
+++ app/src/main/java/com/jns/codelink/activities/MainActivity.kt
@@ -2,7 +2,10 @@ package com.jns.codelink.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jns.codelink.R
+import com.jns.codelink.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val addedProjectsFragment=AddedProjectsFragment()
+        val chatFragment=ChatFragment()
+        val notificationFragment=NotificationFragment()
+        val profileFragment=ProfileFragment()
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val swipeFragment=SwipeFragment()
+
+        setCurrentFragment(swipeFragment)
+        bottomNavigationView.getMenu().findItem(R.id.m_swipe).setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.m_added->setCurrentFragment(addedProjectsFragment)
+                R.id.m_chat->setCurrentFragment(chatFragment)
+                R.id.m_swipe->setCurrentFragment(swipeFragment)
+                R.id.m_notification->setCurrentFragment(notificationFragment)
+                R.id.m_profile->setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout,fragment)
+            commit()
+        }
 }
--- app/src/main/java/com/jns/codelink/fragments/SwipeFragment.kt
+++ app/src/main/java/com/jns/codelink/fragments/SwipeFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [swipeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class swipeFragment : Fragment() {
+class SwipeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class swipeFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            swipeFragment().apply {
+            SwipeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
