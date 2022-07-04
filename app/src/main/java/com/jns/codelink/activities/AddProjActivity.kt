4e9e98dcc338763dd43a5eb4ecd62b6f6e730b2b
Jisha <jishaja14@gmail.com>
2022-07-04 09:46:11 +0530

    AddProjActivity Design

---
+++ app/src/main/java/com/jns/codelink/activities/AddProjActivity.kt
@@ -0,0 +1,12 @@
+package com.jns.codelink.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jns.codelink.R
+
+class AddProjActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_proj)
+    }
+}
