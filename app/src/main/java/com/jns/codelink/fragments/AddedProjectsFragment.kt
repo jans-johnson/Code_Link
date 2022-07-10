40d43d8f3688d424389fec58f13ab3bcb0154d06
Jans Johnson <jaansjohnson@gmail.com>
2022-07-10 14:36:45 +0530

    Updated Added Projects Fragment

--- app/src/main/java/com/jns/codelink/fragments/AddedProjectsFragment.kt
+++ app/src/main/java/com/jns/codelink/fragments/AddedProjectsFragment.kt
@@ -1,60 +1,61 @@
 package com.jns.codelink.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableLayout
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.jns.codelink.R
+import com.jns.codelink.activities.AddProjActivity
+import com.jns.codelink.activities.MainActivity
+import com.jns.codelink.adapters.AddedProjectsAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddedProjectsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddedProjectsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_added_projects, container, false)
-    }
+        val view= inflater.inflate(R.layout.fragment_added_projects, container, false)
+        val tlProjects=view.findViewById<TabLayout>(R.id.tlProjects)
+        val vpAddedProjects=view.findViewById<ViewPager>(R.id.vpAddedProjects)
+        val btnAddProject=view.findViewById<Button>(R.id.btnAddProject)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddedProjectsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddedProjectsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        btnAddProject.setOnClickListener {
+            val intent = Intent(activity, AddProjActivity::class.java)
+            startActivity(intent)
+        }
+
+        val adapter = activity?.supportFragmentManager?.let { AddedProjectsAdapter(it, tlProjects.tabCount) }
+        vpAddedProjects.adapter = adapter
+
+        vpAddedProjects.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tlProjects))
+
+
+        tlProjects.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    vpAddedProjects.currentItem = tab.position
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+
+        return view
     }
+
 }
