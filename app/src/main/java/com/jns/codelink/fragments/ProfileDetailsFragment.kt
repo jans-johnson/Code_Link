2a86abbf4497a7975364937e49fc887ab614c971
Jans Johnson <jaansjohnson@gmail.com>
2022-07-14 19:59:17 +0530

    ProfileDetailsFragment list view link addition

--- app/src/main/java/com/jns/codelink/fragments/ProfileDetailsFragment.kt
+++ app/src/main/java/com/jns/codelink/fragments/ProfileDetailsFragment.kt
@@ -1,11 +1,12 @@
 package com.jns.codelink.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.*
+import androidx.fragment.app.Fragment
 import com.jns.codelink.R
 
 
@@ -14,8 +15,10 @@ class ProfileDetailsFragment : Fragment() {
     lateinit var etDetName:EditText
     lateinit var etDetDesc:EditText
     lateinit var etDetLocation:EditText
-    lateinit var etDetLinks:EditText
     lateinit var etDetSkills:EditText
+    lateinit var lvLinks:ListView
+    lateinit var ivAddLink:ImageView
+    lateinit var linksList:ArrayList<String>
 
 
     override fun onCreateView(
@@ -28,8 +31,36 @@ class ProfileDetailsFragment : Fragment() {
         etDetName=view.findViewById(R.id.etDetName)
         etDetDesc=view.findViewById(R.id.etDetDesc)
         etDetLocation=view.findViewById(R.id.etDetLocation)
-        etDetLinks=view.findViewById(R.id.etDetLinks)
         etDetSkills=view.findViewById(R.id.etDetSkills)
+
+        lvLinks=view.findViewById(R.id.lvDetLinks)
+        ivAddLink=view.findViewById(R.id.ivDetAddLink)
+
+        linksList= ArrayList()
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(requireActivity(), R.layout.links_single_row,R.id.tvLinkStyle, linksList)
+        lvLinks.adapter=adapter
+
+        ivAddLink.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+            builder.setTitle("Enter the link")
+
+            val customLayout: View = layoutInflater
+                .inflate(
+                    R.layout.dialog_get_link,
+                    null
+                )
+            builder.setView(customLayout)
+            builder.setPositiveButton(
+                    "Add"
+                ) { dialog, which ->
+                val editText = customLayout.findViewById<EditText>(R.id.etDialogLink)
+                    linksList.add(editText.text.toString())
+                    adapter.notifyDataSetChanged()
+                }
+            val dialog = builder.create()
+            dialog.show()
+        }
 
         return view
     }
@@ -41,7 +72,7 @@ class ProfileDetailsFragment : Fragment() {
         details["name"] = etDetName.text.toString()
         details["description"]=etDetDesc.text.toString()
         details["location"]=etDetLocation.text.toString()
-        details["links"]=etDetLinks.text.toString()
+        details["links"]=linksList.toString()
         details["skills"]=etDetSkills.text.toString()
         return details
     }
