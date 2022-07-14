17c2ca1c0eb84e3aa93feb9d5b65e6b6924be429
Jans Johnson <jaansjohnson@gmail.com>
2022-07-14 22:29:49 +0530

    Linked Links Card with database

--- app/src/main/java/com/jns/codelink/fragments/ProfileFragment.kt
+++ app/src/main/java/com/jns/codelink/fragments/ProfileFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -15,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.jns.codelink.R
 import com.jns.codelink.models.User
+import kotlin.collections.ArrayList
 
 class ProfileFragment : Fragment() {
 
@@ -25,6 +29,9 @@ class ProfileFragment : Fragment() {
     lateinit var tvProfileDescription:TextView
     lateinit var tvProfileEmail:TextView
     lateinit var tvProfileSkills:TextView
+    lateinit var profileProgressLayout:RelativeLayout
+    lateinit var lvProfileLinks:ListView
+    lateinit var list:ArrayList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,9 +44,13 @@ class ProfileFragment : Fragment() {
         tvProfileDescription=view.findViewById(R.id.tvProfileDescription)
         tvProfileEmail=view.findViewById(R.id.tvProfileEmail)
         tvProfileSkills=view.findViewById(R.id.tvProfileSkills)
+        profileProgressLayout=view.findViewById(R.id.profileProgressLayout)
+        lvProfileLinks=view.findViewById(R.id.lvProfileLinks)
 
         database = FirebaseDatabase.getInstance().reference
         auth = Firebase.auth
+
+        profileProgressLayout.visibility=View.VISIBLE
 
         val userId= auth.currentUser!!.uid
         database.child("users").child(userId).get().addOnSuccessListener {
@@ -49,6 +60,13 @@ class ProfileFragment : Fragment() {
             tvProfileDescription.text=details.description
             tvProfileEmail.text=details.email
             tvProfileSkills.text=details.skills
+
+            val str=details.links.drop(1).dropLast(1)
+            list=ArrayList(str.split(","))
+            val adapter: ArrayAdapter<String> = ArrayAdapter<String>(requireActivity(), R.layout.links_single_row,R.id.tvLinkStyle, list)
+            lvProfileLinks.adapter=adapter
+
+            profileProgressLayout.visibility=View.GONE
             
         }.addOnFailureListener{
             Log.e("firebase", "Error getting data", it)
